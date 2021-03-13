b53a347db41a3bf8a50d94d9ee74a70ad5ca26ed
Sergey Petrov <mavericusdev@gmail.com>
2021-03-13 21:21:23 +0300

    feat: выносит h1 и ul в отдельные render функции

--- src/index.js
+++ src/index.js
@@ -5,19 +5,33 @@ import products from './products.json';
 
 const MAX_COUNT_PRODUCTS_TO_SHOW = 3;
 
+function Title() {
+  return (
+    <h1 className="products-card__title">Список товаров</h1>
+  )
+}
+
+function List(props) {
+  return (
+    <ul className="products-card__list">
+      {props.children}
+    </ul>
+  )
+}
+
 function ProductsCard(props) {
   const productsToShow = props.products.slice(0, MAX_COUNT_PRODUCTS_TO_SHOW);
 
   return (
     <div className="products-card">
-      <h2 className="products-card__title">Список товаров</h2>
-      <ul className="products-card__list">
+      <Title />
+      <List>
         {
           productsToShow.map(({ id, name }) => (
             <li className="products-card__item" key={id}>{name}</li>
           ))
         }
-      </ul>
+      </List>
     </div>
   );
 }
